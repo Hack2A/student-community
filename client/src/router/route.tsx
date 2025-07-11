d2d3475fa0facaf0f5be658ebda8161ff1d13bb7
Akshat Pratyush <akshatp0505@gmail.com>
2025-07-12 00:32:04 +0530

    Error page and Basic Routes Setup

--- client/src/router/route.tsx
+++ client/src/router/route.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from '../pages/landing';
+import ErrorPage from '../pages/error';
 
 const AppRoutes = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Landing />} />
+                <Route path="/u/auth" element={<Landing />} />
+                <Route path="/u/:userName" element={<Landing />} />
+                <Route path="/hackathons" element={<Landing />} />
+                <Route path="/teams" element={<Landing />} />
+                <Route path="/roadmaps" element={<Landing />} />
+
+                {/* Error page for all unhandled routes */}
+                <Route path='*' element={<ErrorPage />} />
             </Routes>
         </Router>
     )
